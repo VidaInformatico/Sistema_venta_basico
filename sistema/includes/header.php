2af4dbeb03723f9e39503c067c65f61f973d8937
Angel Sifuentes <71534078+VidaInformatico@users.noreply.github.com>
2021-02-22 16:19:47 -0500

    Update header.php

--- sistema/includes/header.php
+++ sistema/includes/header.php
@@ -4,6 +4,34 @@ if (empty($_SESSION['active'])) {
 	header('location: ../');
 }
 include "includes/functions.php";
+include "../conexion.php";
+// datos Empresa
+$dni = '';
+$nombre_empresa = '';
+$razonSocial = '';
+$emailEmpresa = '';
+$telEmpresa = '';
+$dirEmpresa = '';
+$igv = '';
+
+$query_empresa = mysqli_query($conexion, "SELECT * FROM configuracion");
+$row_empresa = mysqli_num_rows($query_empresa);
+if ($row_empresa > 0) {
+	if ($infoEmpresa = mysqli_fetch_assoc($query_empresa)) {
+		$dni = $infoEmpresa['dni'];
+		$nombre_empresa = $infoEmpresa['nombre'];
+		$razonSocial = $infoEmpresa['razon_social'];
+		$telEmpresa = $infoEmpresa['telefono'];
+		$emailEmpresa = $infoEmpresa['email'];
+		$dirEmpresa = $infoEmpresa['direccion'];
+		$igv = $infoEmpresa['igv'];
+	}
+}
+$query_data = mysqli_query($conexion, "CALL data();");
+$result_data = mysqli_num_rows($query_data);
+if ($result_data > 0) {
+	$data = mysqli_fetch_assoc($query_data);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
